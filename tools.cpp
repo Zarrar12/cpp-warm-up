87d9445a78ece53405c61941b89c95b89a423a71
zeishan-hasan <zeishan.hasan@gmail.com>
2023-02-20 02:49:02 +0500

    Comment correction

--- tools.cpp
+++ tools.cpp
@@ -1,7 +1,7 @@
 #include "tools.hpp" // "" since the header is not in the default path
 #include <iostream>
 
-// Funciton declaration
+// Funciton definition
 void greet() {
     std::cout << "Welcome to the build system\n";
 }
